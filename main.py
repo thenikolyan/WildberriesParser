12ed4a19bbe03ebee6f58f0c23f16d7b3525631b
Nikolyan <kokashima239@gmail.com>
2023-06-28 15:38:27 +0400

    - add flag 'avaliable' for goods

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # product.to_excel('product.xlsx', index=False)
 
     s = time.time()
-    df = products.multiprocess(pd.DataFrame([{'id': 158112592}]), mode='other_sellers', recollection_=True)
+    df = products.multiprocess(pd.DataFrame([{'id': 67824893}, {'id': 163990051}, {'id':  28918982}]), mode='other_sellers', recollection_=True, avaliable=False)
     e = time.time()
     print(e-s, df.shape)
 
@@ -29,4 +29,3 @@ if __name__ == '__main__':
 
     # catalog = Catalog() 26191563
     # catalog.ge_catalog().to_excel('test.xlsx', index=False)
-
--- utils/products.py
+++ utils/products.py
@@ -44,9 +44,9 @@ class Product(Catalog):
         return limit, pages
 
 
-    def get_data_by_articuls(self, articuls: list):
+    def get_data_by_articuls(self, articuls: list, avaliable: bool=True):
 
-        data = pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+        df = pd.DataFrame([])
 
         limit = 1 if len(articuls) < 100 else int(len(articuls)/100)+1
 
@@ -57,17 +57,27 @@ class Product(Catalog):
             url = 'https://card.wb.ru/cards/detail?appType=1&curr=rub&dest=-1257786&regions=80,38,4,64,83,33,68,70,69,30,86,75,40,1,66,110,22,31,48,71,114&nm=' + tmp_articuls
             response = requests.get(url, headers={'User-Agent': f'{UserAgent().random}'})
 
-            data = pd.concat([data, pd.DataFrame(json.loads(response.text)['data']['products'])])
+            df = pd.concat([df, pd.DataFrame(json.loads(response.text)['data']['products'])])
 
-        data[['priceU', 'salePriceU', 'logisticsCost']] = data[['priceU', 'salePriceU', 'logisticsCost']] / 100
+        if not df.empty:
+            df[['priceU', 'salePriceU', 'logisticsCost']] = df[['priceU', 'salePriceU', 'logisticsCost']] / 100
+        else:
+            return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
 
-        return data[['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId']]
+        # if card exsists,  but nol avaliable
+        if not avaliable:
+            try:
+                df = df.dropna(subset='wh')
+            except KeyError:
+                return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+
+        return df[['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId']]
 
 
-    def get_slice_of_brand_products(self, pages: list, send_end, url: str) -> None:
+    def get_slice_of_brand_products(self, pages: list, send_end, url: str, avaliable: bool=True) -> None:
     
         brandsID = self.get_brand_id(self.url)
-        df = pd.DataFrame(columns = ['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+        df = pd.DataFrame([])
 
         for x in pages:
 
@@ -83,13 +93,24 @@ class Product(Catalog):
             except json.decoder.JSONDecodeError:
                 pass
 
-        df[['priceU', 'salePriceU', 'logisticsCost']] = df[['priceU', 'salePriceU', 'logisticsCost']] / 100
-        send_end.send(df)
+        if not df.empty:
+            df[['priceU', 'salePriceU', 'logisticsCost']] = df[['priceU', 'salePriceU', 'logisticsCost']] / 100
+        else:
+            return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+
+        # if card exsists,  but nol avaliable
+        if not avaliable:
+            try:
+                df = df.dropna(subset='wh')
+            except KeyError:
+                return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+
+        send_end.send(df[['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId']])
 
 
-    def get_slice_of_catalog_products(self, pie: pd.DataFrame, pages: list, send_end, url: str) -> None:
+    def get_slice_of_catalog_products(self, pie: pd.DataFrame, pages: list, send_end, url: str, avaliable: bool=True) -> None:
 
-        df = pd.DataFrame(columns = ['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+        df = pd.DataFrame([])
 
         for x in pages:
             
@@ -105,8 +126,19 @@ class Product(Catalog):
             except json.decoder.JSONDecodeError:
                 pass
 
-        df[['priceU', 'salePriceU', 'logisticsCost']] = df[['priceU', 'salePriceU', 'logisticsCost']] / 100
-        send_end.send(df)
+        if not df.empty:
+            df[['priceU', 'salePriceU', 'logisticsCost']] = df[['priceU', 'salePriceU', 'logisticsCost']] / 100
+        else:
+            return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+
+        # if card exsists,  but nol avaliable
+        if not avaliable:
+            try:
+                df = df.dropna(subset='wh')
+            except KeyError:
+                return pd.DataFrame(columns=['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId'])
+
+        send_end.send(df[['id', 'name', 'brand', 'brandId', 'priceU', 'salePriceU', 'logisticsCost', 'rating', 'feedbacks', 'supplierId']])
 
 
     def get_slice_of_purchased_products(self, products: list, send_end, url: str=None) -> None:
@@ -117,7 +149,7 @@ class Product(Catalog):
         send_end.send(pd.DataFrame(json.loads(response.text)))
 
 
-    def get_slice_of_other_sellers(self,  recollection: bool, articul: list, send_end, url: str=None) -> pd.DataFrame:
+    def get_slice_of_other_sellers(self,  recollection: bool, articul: list, send_end, url: str=None, avaliable: bool=True) -> pd.DataFrame:
 
         df = pd.DataFrame(columns=['id', 'brother'])
         SQLarticul = get_articuls(articul)
@@ -148,14 +180,14 @@ class Product(Catalog):
         SQLarticul = SQLarticul[SQLarticul.id.isin(list(unchangable_articul))].reset_index(drop=True)
 
 
-        ids = self.get_data_by_articuls(list(set(list(SQLarticul.id.astype('int').astype('str')))))
-        brothers = self.get_data_by_articuls(list(set(list(SQLarticul.brother.astype('int').astype('str')))))
+        ids = self.get_data_by_articuls(list(set(list(SQLarticul.id.astype('int').astype('str')))), avaliable)
+        brothers = self.get_data_by_articuls(list(set(list(SQLarticul.brother.astype('int').astype('str')))), avaliable)
 
         tmp = [SQLarticul, ids, brothers]
         send_end.send(tmp)
 
 
-    def get_sellers_name(self, sellersID: list, send_end, url: str=None) -> pd.DataFrame:
+    def get_sellers_name(self, sellersID: list, send_end, url: str=None, avaliable: bool=True) -> pd.DataFrame:
         sellersID = set(sellersID)
         sellers = get_sellers()
 
@@ -179,42 +211,42 @@ class Product(Catalog):
         send_end.send(sellers)
 
 
-    def add_seller_names(self, df: pd.DataFrame, n_proc: int=4) -> pd.DataFrame:
+    def add_seller_names(self, df: pd.DataFrame, n_proc: int=4, avaliable: bool=True) -> pd.DataFrame:
 
         sellers = list(set(list(df.supplierId)))
         limit, pages = self.number_of_items(len(sellers), n_proc)
-        sellers_pipe_list = self.create_loop(self.get_sellers_name, limit, sellers, n_proc)
+        sellers_pipe_list = self.create_loop(target=self.get_sellers_name, limit=limit, data=sellers, n_proc=n_proc, avaliable=avaliable)
         sellers = pd.concat([x.recv() for x in sellers_pipe_list])
 
         return df.merge(sellers.drop_duplicates(keep='first').rename(columns={'id': 'supplierId', 'name': 'seller_name'}), on='supplierId', how='left')
 
 
-    def create_loop(self, target, limit: int, data: list, n_proc: int, added_data: any=None) -> list:
+    def create_loop(self, target, limit: int, data: list, n_proc: int, avaliable: bool=True, added_data: any=None) -> list:
         jobs, pipe_list = [], []
         if added_data is None:
             for i in range(n_proc):
                         recv_end, send_end = multiprocessing.Pipe(True)
-                        process = multiprocessing.Process(target=target, args=(data[limit*i:limit*(i+1)], send_end, self.url))
+                        process = multiprocessing.Process(target=target, args=(data[limit*i:limit*(i+1)], send_end, self.url, avaliable))
                         jobs.append(process)
                         pipe_list.append(recv_end)
                         process.start()
-        else: 
+        else:
             for i in range(n_proc):
                         recv_end, send_end = multiprocessing.Pipe(True)
-                        process = multiprocessing.Process(target=target, args=(added_data, data[limit*i:limit*(i+1)], send_end, self.url))
+                        process = multiprocessing.Process(target=target, args=(added_data, data[limit*i:limit*(i+1)], send_end, self.url, avaliable))
                         jobs.append(process)
                         pipe_list.append(recv_end)
                         process.start()
         return pipe_list
 
 
-    def multiprocess(self, var, mode: str=None, recollection_: bool=False) -> pd.DataFrame:
+    def multiprocess(self, var, mode: str=None, recollection_: bool=False, avaliable: bool=True) -> pd.DataFrame:
         n_proc = multiprocessing.cpu_count()
 
         if isinstance(var, int):
             if 'brands' in self.url:
                 limit, pages = self.number_of_items(var, n_proc)
-                pipe_list = self.create_loop(self.get_slice_of_brand_products, limit, pages, n_proc)
+                pipe_list = self.create_loop(target=self.get_slice_of_brand_products, limit=limit, data=pages, n_proc=n_proc, avaliable=avaliable)
 
             elif 'catalog' in self.url:
 
@@ -222,7 +254,7 @@ class Product(Catalog):
                 pie = pie.query(f'''url == "{self.url.split('www.wildberries.ru')[-1]}"''')
 
                 limit, pages = self.number_of_items(var, n_proc)
-                pipe_list = self.create_loop(self.get_slice_of_catalog_products, limit, pages, n_proc, pie)
+                pipe_list = self.create_loop(target=self.get_slice_of_catalog_products, limit=limit, data=pages, n_proc=n_proc, avaliable=avaliable, added_data=pie)
             else:
                 print('Mode is not mean')
                 return pd.DataFrame([])
@@ -243,7 +275,7 @@ class Product(Catalog):
 
             if mode == 'other_sellers':
 
-                pipe_list = self.create_loop(target=self.get_slice_of_other_sellers, limit=limit, data=list(var.id), n_proc=n_proc, added_data=recollection)
+                pipe_list = self.create_loop(target=self.get_slice_of_other_sellers, limit=limit, data=list(var.id), n_proc=n_proc, avaliable=avaliable, added_data=recollection)
 
                 if recollection_:
                     recollection(flag=recollection_, table='articuls')
@@ -253,17 +285,21 @@ class Product(Catalog):
                 ids = pd.concat([x[1] for x in listdf]).drop_duplicates(keep='first')
                 brothers = pd.concat([x[2] for x in listdf]).drop_duplicates(keep='first')
 
-                SQLarticul = SQLarticul.merge(self.add_seller_names(ids, n_proc).drop(columns=['fineName', 'ogrn', 'trademark', 'legalAddress', 'isUnknown']), on='id', how='left')
-                SQLarticul = SQLarticul.merge(self.add_seller_names(brothers, n_proc).drop(columns=['fineName', 'ogrn', 'trademark', 'legalAddress', 'isUnknown']).rename(columns={'id': 'brother'}), on='brother', how='left', suffixes=('', '_brother'))
+                SQLarticul = SQLarticul.merge(self.add_seller_names(df=ids, n_proc=n_proc, avaliable=avaliable).drop(columns=['fineName', 'ogrn', 'trademark', 'legalAddress', 'isUnknown']), on='id', how='left')
+                SQLarticul = SQLarticul.merge(self.add_seller_names(df=brothers, n_proc=n_proc, avaliable=avaliable).drop(columns=['fineName', 'ogrn', 'trademark', 'legalAddress', 'isUnknown']).rename(columns={'id': 'brother'}), on='brother', how='left', suffixes=('', '_brother'))
 
                 SQLarticul = SQLarticul.dropna()
 
                 return SQLarticul
 
             elif mode == 'purchased_products':
-                pipe_list = self.create_loop(self.get_slice_of_purchased_products, limit, list(var.id), n_proc)
+                pipe_list = self.create_loop(target=self.get_slice_of_purchased_products, limit=limit, data=list(var.id), n_proc=n_proc, avaliable=avaliable)
             else:
                 print('Mode is not mean')
                 return pd.DataFrame([])
 
             return pd.concat([x.recv() for x in pipe_list])
+        
+        else:
+            print('Mode is not mean')
+            return pd.DataFrame([])
